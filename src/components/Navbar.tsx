fd7baac11723fe4f13f45b7d1b40a31dde1eb3f7
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-08-03 13:06:40 -0400

    Try this

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import { toggleModal } from "../redux/slices/wordSlice";
 import { useAppDispatch } from "../redux/store";
 import ThemeToggle from "./ThemeToggle";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
 	const dispatch = useAppDispatch();
 	return (
 		<header className="border-b-black/50 border-b-2 h-12 sm:h-14 w-full">
-			<button onClick={() => dispatch(toggleModal())}>MODAL</button>
+			<button
+				onClick={() => dispatch(toggleModal())}
+				className="text-3xl absolute top-2 md:top-3 left-2"
+			>
+				<GiHamburgerMenu />
+			</button>
 			<h1 className="absolute top-1 left-[50%] -translate-x-[50%] text-2xl md:text-4xl font-bold">
 				VARI-WORDLE
 			</h1>
--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -1,13 +1,22 @@
+import { useRef } from "react";
 import { toggleModal } from "../../redux/slices/wordSlice";
 import { useAppDispatch } from "../../redux/store";
 
 const Modal = () => {
 	const dispatch = useAppDispatch();
+	const ref = useRef<HTMLDivElement>(null);
 	return (
 		<div
 			className="absolute z-20 top-0 bottom-0 right-0 left-0 bg-gray-600/50"
-			onClick={() => dispatch(toggleModal())}
-		></div>
+			ref={ref}
+			onClick={(e) => {
+				if (e.target === ref.current) dispatch(toggleModal());
+			}}
+		>
+			<div className="absolute z-30 top-10 bottom-10 left-4 right-4 md:left-[50%] md:-translate-x-[50%] md:w-[500px] bg-slate-800 rounded-md">
+				WIP - click outside to close
+			</div>
+		</div>
 	);
 };
 
