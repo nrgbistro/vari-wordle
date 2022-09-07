f2919a83d7b15dd86389f086dca419173a1612e9
nrgbistro <nolangelinas@gmail.com>
2022-09-07 14:55:22 -0400

    Add filtered words list for client side word check

---
+++ server/filterWords.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./words_alpha.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+
+rl.on('line', (line) => {
+    if(line.length >= 4 && line.length <= 8){
+        fs.appendFile('words_filtered.txt', line + "\n", function (err) {
+            if (err) throw err;
+          });
+    }
+});
