e8a939618c029d55fda1c95da7590778125768d4
nrgbistro <nolangelinas@gmail.com>
2023-05-30 14:07:11 -0400

    Remove mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-jest.mock("axios");
-
 test("renders VARI-WORDLE title", () => {
 	render(<App />);
 	const linkElement = screen.getByText(/VARI-WORDLE/i);
