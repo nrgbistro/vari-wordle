775bf23a3b7fe8d81aab35f5e708d2ef7c875470
nrgbistro <nolangelinas@gmail.com>
2023-10-21 00:26:48 -0400

    Complete test coverage

--- server/test/gameHelpers.test.ts
+++ server/test/gameHelpers.test.ts
@@ -1,8 +1,15 @@
-import { generateNewWordHelper } from "../src/gameHelpers";
+import { generateNewWord, generateNewWordHelper } from "../src/gameHelpers";
 
 describe("Game Helpers", () => {
 	it("generates a word", async () => {
 		const word = await generateNewWordHelper();
-		expect(typeof word).toEqual("string");
+		expect(typeof word).toBe("string");
+	});
+
+	it("generates a word with a new count", async () => {
+		const word = await generateNewWord(5, false);
+		console.log(word);
+		expect(word.length).toBeGreaterThan(3);
+		expect(word.length).toBeLessThan(9);
 	});
 });
